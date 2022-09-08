7e3a3e30679db28fa692dc75037c9b03bf7f92fb
한채은 <72204267+Hanchaeeun@users.noreply.github.com>
2022-09-09 00:34:30 +0900

    Update function

--- Top-biomarker Testing @ Chaeeun/plot_accuracy.py
+++ Top-biomarker Testing @ Chaeeun/plot_accuracy.py
@@ -119,9 +119,6 @@ def plot_stepwise_accuracy(df, ranking_df, step_num, model, accuracy_metric, mul
                 # accuracy_metric
                 if multi_class == True:
                     acc.append(metrics.accuracy_score(y_test, y_pred))
-                    pre.append(metrics.precision_score(y_test, y_pred))
-                    f1.append(metrics.f1_score(y_test, y_pred))
-                    recall.append(metrics.recall_score(y_test, y_pred))
                 else:
                     f1.append(metrics.f1_score(y_test, y_pred))
                     acc.append(metrics.accuracy_score(y_test, y_pred))
@@ -131,7 +128,7 @@ def plot_stepwise_accuracy(df, ranking_df, step_num, model, accuracy_metric, mul
                     aic.append(2*metrics.log_loss(y_test, y_proba) + 2*num)
                     bic.append(2*metrics.log_loss(y_test, y_proba) + np.log(x_test.shape[0])*num)   
                 if multi_class == True:
-                    mean_list, cols = [np.mean(f1), np.mean(acc), np.mean(pre), np.mean(recall)], ['f1', 'accuracy', 'precision', 'recall']
+                    mean_list, cols = [np.mean(acc)], ['accuracy']
                 else:
                     mean_list, cols = [np.mean(f1), np.mean(acc), np.mean(pre), np.mean(recall), np.mean(roc), np.mean(aic), np.mean(bic)], ['f1', 'accuracy', 'precision', 'recall', 'roc', 'aic', 'bic']
             score_step = pd.DataFrame([mean_list], columns=cols)     
